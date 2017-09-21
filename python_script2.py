799da0feafa17da4a4c1e8928ebf6f1a6e8e1c9b
AlexDataScientist <alexandre.garcia.stat@gmail.com>
2017-09-21 10:26:48 +0200

    Add comments in python_script2.py

--- python_script2.py
+++ python_script2.py
@@ -2,9 +2,15 @@
 Encore super simple!
 """
 
+# Some imports
 import numpy as np
 import pandas as pd
 
+# create a matrix 2 x 2
 matrix = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Create a dataframe
 data_frame = pd.DataFrame(matrix)
+
+# Print stats
 print("Voici des stats de bases: {}".format(data_frame.describe()))
