c98f1566ef973885abcd3134fde912404f555f4d
AlexDataScientist <alexandre.garcia.stat@gmail.com>
2017-09-21 10:25:09 +0200

    Change la valeur de la variable a et comparaison des valeurs absolues dans python_script1.py

--- python_script1.py
+++ python_script1.py
@@ -1,9 +1,12 @@
 """
 Super simple!
 """
+import numpy as np
  
-a = 4 
+a = -8 
 b = 6
 
 if a<b:
   print("Salut mec!") 
+if np.abs(a) > np.abs(b):
+  print("Valeur absolu de {} plus grande que valeur absolu de {}".format(a, b))
